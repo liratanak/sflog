58fd77108d31c3d808b8fb30ffb53ae2873e0e87
liratanak <liratanak.ann@gaea-sys.com>
2015-09-08 18:50:13 +0700

    Step1: Get New ID tail for new logs ...

--- sflog.js
+++ sflog.js
@@ -1,2 +1,21 @@
-var jsforce = require('jsforce');
+/**
+ * SFlog
+ */
 
+var force = require('./lib/force');
+var log = require('captains-log')();
+var config = require('./config.json');
+var SFLog = require('./lib/sflog')
+
+log.info('Autenticating...');
+force.auth.login({
+	username: config.org.username,
+	passwordToken: config.org.password + config.org.secretToken,
+}).then(function( authConnection ) {
+	log.info('Autenticated');
+	return new SFLog({
+		connection: force.auth.connection
+	}).tail();
+}).catch(function( e ) {
+	log.error( e );
+});
