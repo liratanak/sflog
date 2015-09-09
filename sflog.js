96ef32fecf544899454eba264a55ea4526fc3efe
liratanak <liratanak.ann@gaea-sys.com>
2015-09-09 12:23:34 +0700

    Step2: Usable individually tail for new logs body working with grep

--- sflog.js
+++ sflog.js
@@ -3,7 +3,7 @@
  */
 
 var force = require('./lib/force');
-var log = require('captains-log')();
+var log = require('./lib/util/log');
 var config = require('./config.json');
 var SFLog = require('./lib/sflog')
 
@@ -12,9 +12,11 @@ force.auth.login({
 	username: config.org.username,
 	passwordToken: config.org.password + config.org.secretToken,
 }).then(function( authConnection ) {
-	log.info('Autenticated');
+	log.silly( 'authConnection: ', authConnection );
+	log.info('Autenticated as ' + config.org.username + ' [ID: ' + authConnection.userInfo.id + ' ]' );
 	return new SFLog({
-		connection: force.auth.connection
+		connection: force.auth.connection,
+		timeout: config.timeout
 	}).tail();
 }).catch(function( e ) {
 	log.error( e );
